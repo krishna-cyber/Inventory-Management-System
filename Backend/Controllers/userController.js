7f3ec2e6325a6d2656a9e0e68673a3e79b86b129
krishna tiwari <tiwarikrishna54321@gmail.com>
2023-01-20 12:07:09 +0545

    user login route and logic added

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -12,7 +12,8 @@ const generateToken = (id) => {
   });
 };
 //controllers
-const registeruser = asyncHandler(async (req, res, next) => {
+//user registration controller
+const registerUser = asyncHandler(async (req, res, next) => {
   const { name, email, password, phone } = req.body;
   console.log(req.body);
 
@@ -69,7 +70,57 @@ const registeruser = asyncHandler(async (req, res, next) => {
     throw new Error("Invalid user data");
   }
 });
+
+//user login controller
+const loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  //validation
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please fill all required fields");
+  }
+  //check if the user exists
+  const user = await User.findOne({
+    email,
+  });
+  if (!user) {
+    res.status(400);
+    throw new Error("User does not exist! Please Sign up first💖");
+  }
+  //check if the password is correct
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  //sending response to client after password correct and user found
+  if (isPasswordCorrect && user) {
+    const { _id, name, email, photo, phone, bio } = user;
+
+    //generate token for the user
+    const token = generateToken(_id); //Token will expire in 1 day and generated for the user._id
+    //set cookie
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000), //cookie will expire in 1 day
+      sameSite: "none", //for different sites
+      secure: true, //for secure cookie
+    });
+
+    //send response
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+      token,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password!!");
+  }
+});
 // exporting controllers
 module.exports = {
-  registeruser,
+  registerUser,
+  loginUser,
 };
--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
 //importing controllers
-const { registeruser } = require("../Controllers/userController");
+const { registerUser, loginUser } = require("../Controllers/userController");
 
 //creating router object
 const router = express.Router();
 
 //creating routes
-router.post("/register", registeruser); //register user
+router.post("/register", registerUser); //register user
+router.post("/login", loginUser);
 
 //exporting routes
 module.exports = router; // exporting router object
