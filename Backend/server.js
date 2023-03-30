ff964290100daccacbda65087be1e9b95a152c4a
krishna tiwari <tiwarikrishna54321@gmail.com>
2023-03-30 15:14:09 +0545

    server instance created|

--- Backend/server.js
+++ Backend/server.js
@@ -1,41 +1,17 @@
-//importing modules and files
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser"); //importing cookie parser
+//configuring dotenv
 require("dotenv").config();
-const errorHandlingMW = require("./middlewares/errorHandlingMW");
 
-//importing routes
-const userRoute = require("./Routes/userRoute");
+const express = require("express");
+const mongoose = require("mongoose");
 
-//creating server
-const app = express(); //creating express app
+//creating app instance
+const app = express();
 
-//Middleware
-app.use(cors()); //to allow cross origin resource sharing
-app.use(bodyParser.json()); // for parsing application/json data come from frontend to backend
-app.use(express.json()); //parse application/json
-app.use(express.urlencoded({ extended: false })); //parse application/x-www-form-urlencoded
-app.use(cookieParser()); //using cookie parser
-app.use(errorHandlingMW); //error handling middleware
-//providing port for server
-const PORT = process.env.PORT || 5000;
+//connection with database and server start
 
-//Routes middleware
-app.use("/api/user", userRoute);
-
-//connect to database and start server
-mongoose
-  .connect(process.env.DB_CONNECT, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log(`Connected to database 🪨`);
-    app.listen(PORT, () => {
-      console.log(`Server started on port ${PORT} 🚀`);
-    });
-  })
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI).then((conn) => {
+  console.log(`Database connected at instance ${conn.connection.host}`);
+  app.listen(process.env.PORT, () => {
+    console.log(`Server started at port ${process.env.PORT}`);
+  });
+});
